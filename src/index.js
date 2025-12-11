07ffe59e98a9947b56890ddb359e7bbcc7b44a6e
uznza <uznzafer@gmail.com>
2025-12-11 21:07:23 +0300

    basic combine reducer added

--- src/index.js
+++ src/index.js
@@ -6,65 +6,116 @@ import AppRouter from './routers/AppRouter';
 import './index.css';
 
 
-import {createStore} from 'redux'
+import {createStore, combineReducers} from 'redux'
 
-//store uygulamanın conteiner gibi düşünülebilir. Tek bir yerden state bilgileri almak için tanımlıyoruz.
-const initialStore ={
-  count:0
+// //store uygulamanın conteiner gibi düşünülebilir. Tek bir yerden state bilgileri almak için tanımlıyoruz.
+// const initialStore ={
+//   count:0
+// }
+
+// //REDUCERS
+// //spesifik reducer tanımlaması,store içerisinde birden fazla reducer içerebilir
+// const counterReducer = (state = initialStore,action)=>{
+//   switch(action.type){
+//     case"INCREMENT":
+//       return {
+//           count:state.count+1
+//       }
+//     case"DECREMENT":
+//       return {
+//           count:state.count-1
+//       }    
+//     case"RESET":
+//       return {
+//           count:0
+//       }
+//     default:
+//       return state;
+//   }
+//   return state;
+// }
+
+
+// //set store
+// //başlangıçta bir state bilgisi var bunu ortak bir store'da paylaşıyoruz. daha sonra bu bilgiyi getirmek ve degiştirmek için action methodları belirliyoruz.
+// //bu methodlarla store içinde degişiklikler yapılıp state bilgisini dönüyor.
+// const store = createStore(counterReducer);
+
+// //dispatch her çalıştıgında çalışır
+// store.subscribe(()=>{
+//   console.log("subs " +store.getState().count);//count 2
+// })
+
+// //get state
+
+// console.log(store.getState());
+
+// //INCREMENT, DECREMENT, RESET => ACTIONS (Object)
+
+// //DISPATCH 
+// store.dispatch({
+//   type:"INCREMENT"
+// })
+// console.log(store.getState()); //count 1 
+
+// store.dispatch({
+//   type:"INCREMENT"
+// })
+// console.log(store.getState());//count 2
+
+
+const state =  {
+  blogs:[{
+      id:1,
+      title:'blog title 1',
+      description: 'blog description 1',
+      dateAdded:0
+  }],
+  auth:{
+    userid:1,
+    username:'zuzu',
+    email:'info@email.com'
+  }
 }
+// ACTION CREATOR
 
-//REDUCERS
-//spesifik reducer tanımlaması,store içerisinde birden fazla reducer içerebilir
-const counterReducer = (state = initialStore,action)=>{
-  switch(action.type){
-    case"INCREMENT":
-      return {
-          count:state.count+1
-      }
-    case"DECREMENT":
-      return {
-          count:state.count-1
-      }    
-    case"RESET":
-      return {
-          count:0
-      }
+const blogState= [{
+      id:1,
+      title:'blog title 1',
+      description: 'blog description 1',
+      dateAdded:0
+  }];
+
+const blogReducer = (state=blogState,action ) =>{
+
+  switch (action.type){
     default:
       return state;
   }
-  return state;
 }
 
 
-//set store
-//başlangıçta bir state bilgisi var bunu ortak bir store'da paylaşıyoruz. daha sonra bu bilgiyi getirmek ve degiştirmek için action methodları belirliyoruz.
-//bu methodlarla store içinde degişiklikler yapılıp state bilgisini dönüyor.
-const store = createStore(counterReducer);
+const authState=   {
+    userid:1,
+    username:'zuzu',
+    email:'info@email.com'
+  };
 
-//dispatch her çalıştıgında çalışır
-store.subscribe(()=>{
-  console.log("subs " +store.getState().count);//count 2
-})
+const authReducer = (state=authState,action ) =>{
 
-//get state
-
-console.log(store.getState());
-
-//INCREMENT, DECREMENT, RESET => ACTIONS (Object)
-
-//DISPATCH 
-store.dispatch({
-  type:"INCREMENT"
-})
-console.log(store.getState()); //count 1 
-
-store.dispatch({
-  type:"INCREMENT"
-})
-console.log(store.getState());//count 2
+  switch (action.type){
+    default:
+      return state;
+  }
+}
 
 
+const store =  createStore(combineReducers({
+  blogs:blogReducer,
+  auth:authReducer
+}));
 
+console.log(store.getState())
 
 
 
