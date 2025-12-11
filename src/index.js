1d231e94a50f7c51aa7b0033593bf58e37c051f0
uznza <uznzafer@gmail.com>
2025-12-11 18:19:40 +0300

    redux, dispatch, store added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+import AppRouter from './routers/AppRouter';
+import './index.css';
+
+
+import {createStore} from 'redux'
+
+//store uygulamanın conteiner gibi düşünülebilir. Tek bir yerden state bilgileri almak için tanımlıyoruz.
+const initialStore ={
+  count:0
+}
+
+//set store
+//başlangıçta bir state bilgisi var bunu ortak bir store'da paylaşıyoruz. daha sonra bu bilgiyi getirmek ve degiştirmek için action methodları belirliyoruz.
+//bu methodlarla store içinde degişiklikler yapılıp state bilgisini dönüyor.
+const store = createStore((state = initialStore,action)=>{
+  switch(action.type){
+    case"INCREMENT":
+      return {
+          count:state.count+1
+      }
+    case"DECREMENT":
+      return {
+          count:state.count-1
+      }    
+    case"RESET":
+      return {
+          count:0
+      }
+    default:
+      return state;
+  }
+  return state;
+})
+
+//dispatch her çalıştıgında çalışır
+store.subscribe(()=>{
+  console.log("subs " +store.getState().count);//count 2
+})
+
+//get state
+
+console.log(store.getState());
+
+//INCREMENT, DECREMENT, RESET => ACTIONS (Object)
+store.dispatch({
+  type:"INCREMENT"
+})
+console.log(store.getState()); //count 1 
+
+store.dispatch({
+  type:"INCREMENT"
+})
+console.log(store.getState());//count 2
+
+
+
+
+//DISPATCH 
+
+//REDUCERS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//reportWebVitals();
